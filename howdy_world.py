3e60bc12fe42dd772d13d99d5267502fd2611ecb
GavinCunanan <gavincunanan@tamu.edu>
2025-10-26 18:51:20 -0500

    Add files via upload

---
+++ howdy_world.py
@@ -0,0 +1,12 @@
+# By submitting this assignment, I agree to the following:
+# "Aggies do not lie, cheat, or steal, or tolerate those who do."
+# "I have not given or received any unauthorized aid on this assignment."
+#
+# Name: Gavin Cunanan
+# Section: 417/517
+# Assignment: LAB 1.11
+# Date: 25 August 2025
+#
+print("Howdy, World!")
+print("I play basketball and volleyball.")
+ 
