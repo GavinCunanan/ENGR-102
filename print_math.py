a5431ad8bdf957901956c099bc242d963f858349
GavinCunanan <gavincunanan@tamu.edu>
2025-10-26 19:00:57 -0500

    Add files via upload

---
+++ print_math.py
@@ -0,0 +1,25 @@
+# By submitting this assignment, I agree to the following:
+# "Aggies do not lie, cheat, or steal, or tolerate those who do."
+# "I have not given or received any unauthorized aid on this assignment."
+#
+# Name: Gavin Cunanan
+# Section: 417/517
+# Assignment: LAB 1.12.1
+# Date: 26 August 2025
+#
+from math import *
+
+#Renolds Number ((u * L) / v)
+print ("Reynolds number is",(9 * 0.875)/0.0015)
+
+#Braggs Law ((2 * d) * sin(Theta)))
+print("Wavelength is", (2 * 0.029) * sin((radians(35))), "nm")
+
+#Arps equation (q / (1 + (b * d * t)) ** 1 / b)
+print ("Production rate is", (100 / (1 + (0.8 * 2 * 10)) ** (1 / 0.8)), "barrels/day")
+
+#Tsiolvosky rocket equation (v * (log(e) * (m / mf)))
+#AAAAAAA 2029
+print ("Change of velocity is", (2029 * (log(11000 / 8300))), "m/s")
+
+print(sqrt(5))
